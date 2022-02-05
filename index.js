0beb36a63618671a0393dfd5d5f7afc4262c68ac
kevinharv <kevinharv@protonmail.com>
2022-02-04 22:02:55 -0600

    Base login

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
+    const guildList = client.guilds.cache.map(guild => guild.name)
+    console.log(`Bot go online in ${guildList.length}`)
+    console.log(guildList.join('\n'))
+}); 
+
+client.on('message', message => {
+	console.log(message.content);
+    message.reply('Hello World!');
 });
 
 // Login to Discord with your client's token
