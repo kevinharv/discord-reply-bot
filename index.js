803d2cba0d45b89bbc7f227289ca9a7bf57d1d07
Kevin <kevinharv@protonmail.com>
2022-02-05 20:11:38 -0600

    IT WORKS

---
+++ index.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, "GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.on('messageCreate', message => {
+    
+    const messageOwner = message.author;
+    const messageContent = message.content;
+
+    // DEBUG
+    // console.log(`${messageOwner.username} said: ${messageContent}`);
+
+    if (messageOwner.id == '350054304536854529') {
+        message.reply('https://cdn.discordapp.com/attachments/607022384985145345/939325385991159939/unknown.png');
+    }
+});
+
+// Login to Discord with your client's token
+client.login(token);
